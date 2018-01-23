f9055a51c698339d41a9994e386469cef045ed76
AH1534 <ah1534@live.mdx.ac.uk>
2018-01-23 15:16:26 +0000

    added a file to test the behavior of numpy.shape()

---
+++ src/baxter_pick_place_pkg/src/numpy_shape_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import numpy as np
+
+var = [(140, 42), 'CRO', '93.3333333333']
+
+print var
+print np.shape(var)
+
+
+var = [[(69, 116), 'CIR', '100.0'], [(140, 42), 'CRO', '93.3333333333']]
+
+print var
+print np.shape(var)
+
+var = [[(69, 116), 'CIR', '100.0'], [(140, 42), 'CRO', '93.3333333333'], [(140, 42), 'CRO', '93.3333333333']]
+
+print var
+print np.shape(var)
